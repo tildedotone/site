1534985af240cad51c52bc204fca089299b0d526
deepend <deepend@tilde.club>
2023-01-15 23:31:10 +0000

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -80,7 +80,7 @@
     </div>
     <!-- Tildeverse Banner Exchange code end -->
         <p>
-	    Have a question? Ask on the <a href="https://tilde.chat">IRC server</a> in the <u>#one</u> channel or via an issue on the <a href="https://tildegit.org/one/setup/issues/new">git repository</a>
+	    Have a question? Ask on the <a href="https://newnet.net">IRC server</a> in the <u>#one</u> channel or via an issue on the <a href="https://tildegit.org/one/setup/issues/new">git repository</a>
         </p>   
     </footer>
 </body>
